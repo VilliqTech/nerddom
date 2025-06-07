ae0ad88486cb1a921bafd29c025fe95693aa5c91
VilliqTech <dev-team@villiq.org>
2025-06-07 12:45:58 -0400

    chore: Initial commit

---
+++ app.config.timestamp_1749258069049.js
@@ -0,0 +1,17 @@
+// app.config.ts
+import { defineConfig } from '@tanstack/react-start/config';
+import tsConfigPaths from 'vite-tsconfig-paths';
+
+var app_config_default = defineConfig({
+    tsr: {
+        appDirectory: 'app',
+    },
+    vite: {
+        plugins: [
+            tsConfigPaths({
+                projects: ['./tsconfig.json'],
+            }),
+        ],
+    },
+});
+export { app_config_default as default };
